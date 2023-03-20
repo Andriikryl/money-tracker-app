957709d4b6f858846ffb9221c75baf6fc785d8d9
Amdrii <andreyk557@gmail.com>
2023-03-20 16:20:16 +0200

    feat[form]

--- src/App.js
+++ src/App.js
@@ -7,14 +7,49 @@ function App() {
         $400 <span>.00</span>
       </h1>
       <form>
-        <div>
+        <div className="basic">
           <input type="text" placeholder={"+200 nwe samsung tv"} />
           <input type="datetime-local" />
         </div>
         <div className="descriptions">
           <input type="textl" placeholder={"description"} />
         </div>
+        <button type="submit">Add</button>
       </form>
+      <div className="transactions">
+        <div className="transaction">
+          <div className="left">
+            <div className="name">new samsung tv</div>
+            <div className="description">it was time for new tv</div>
+          </div>
+          <div className="right">
+            <div className="price">$400.00</div>
+            <div className="datetime">2022-12-18 15:45</div>
+          </div>
+        </div>
+
+        <div className="transaction">
+          <div className="left">
+            <div className="name">new samsung tv</div>
+            <div className="description">it was time for new tv</div>
+          </div>
+          <div className="right">
+            <div className="price">$400.00</div>
+            <div className="datetime">2022-12-18 15:45</div>
+          </div>
+        </div>
+
+        <div className="transaction">
+          <div className="left">
+            <div className="name">new samsung tv</div>
+            <div className="description">it was time for new tv</div>
+          </div>
+          <div className="right">
+            <div className="price">$400.00</div>
+            <div className="datetime">2022-12-18 15:45</div>
+          </div>
+        </div>
+      </div>
     </main>
   );
 }
